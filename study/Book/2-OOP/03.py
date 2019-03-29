ebe0533095357a3b79458eb54253b8f2c90d3783
jinyitechli <jinyitechli@163.com>
2019-03-29 09:30:40 +0800

    one

---
+++ study/Book/2-OOP/03.py
@@ -0,0 +1,16 @@
+from urllib import request
+import chardet
+
+if __name__ == '__main__':
+
+    url = "http://www.myjinyitech.com/"
+
+    rsp = request.urlopen(url)
+
+    html = rsp.read()
+    cs = chardet.detect(html)
+    print(type(cs))
+    print(cs)
+
+    html = html.decode(cs.get("encoding","utf-8"))
+    print(html)
